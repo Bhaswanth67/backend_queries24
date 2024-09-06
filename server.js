38e782c171540384f09c7be162f07ad876a51de1
Bhaswanth Kumar Bonthala <125963613+Bhaswanth67@users.noreply.github.com>
2024-09-07 01:01:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
 app.use(express.json());
